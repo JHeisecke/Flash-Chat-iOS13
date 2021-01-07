5a27cbed16c1cc4d0d884a85443c0c01b3dbaa8e
Javier Heisecke <javier.heisecke@bbva.com>
2021-01-07 11:10:37 -0300

    Agrega practica con Remote Config

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -8,15 +8,37 @@
 
 import UIKit
 import CLTypingLabel
+import Firebase
 
 class WelcomeViewController: UIViewController {
-
+    
     @IBOutlet weak var titleLabel: CLTypingLabel!
-        
+    @IBOutlet weak var registerButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.titleLabel.text = "⚡️FlashChat"
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        //Remote Config
+        let settings = RemoteConfigSettings()
+        settings.minimumFetchInterval = 30 //segundos que espera firebase para actualizar su configuracion
+        let remoteConfig = RemoteConfig.remoteConfig()
+        remoteConfig.configSettings = settings
+        remoteConfig.setDefaults(fromPlist: "DefaultValues") //seteamos valores por defectos
+        
+        //fetch valores de la nube, si existe alguna pasa a ser vigente y sustituye los valores por defecto
+        remoteConfig.fetchAndActivate { (status, error) in
+            if status != .error {
+                let title = remoteConfig.configValue(forKey: "title_text").stringValue
+                let showRegister = remoteConfig.configValue(forKey: "show_register_button").boolValue
+                DispatchQueue.main.async {
+                    self.titleLabel.text = title
+                    self.registerButton.isHidden = !showRegister
+                }
+            }
+        }
+    }
+
 
 }
